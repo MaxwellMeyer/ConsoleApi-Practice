baaa686f18a2afa7f31d320a22720bf866e60c15
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-02 22:50:07 -0700

    update program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using RestSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ApiTest
 {
@@ -13,17 +14,25 @@ namespace ApiTest
       var apiCallTask = ApiHelper.ApiCall(("..."));
       var result = apiCallTask.Result;
       JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-      Console.WriteLine(jsonResponse["reults"]);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+      foreach (Article article in articleList)
+      {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Byline: {article.Byline}");
+      }
     }
-  }
-  class ApiHelper
-  {
-    public static async Task<string> ApiCall(string apikey)
+    class ApiHelper
     {
-      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
-      RestRequest request = new RestRequest($"home.json?api-key={...}", Method.GET);
-      var response = await client.ExecuteTaskAsync(request);
-      return response.Content;
+      public static async Task<string> ApiCall(string apikey)
+      {
+        RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+        RestRequest request = new RestRequest($"home.json?api-key={...}", Method.GET);
+        var response = await client.ExecuteTaskAsync(request);
+        return response.Content;
+      }
     }
   }
-}
